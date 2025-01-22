51de82e99ccc6ccba4798f75c3dfc2b85500b6b1
B1lol-dev <sardorovichgamer@gmail.com>
2025-01-22 19:54:20 +0500

    update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ export function themeSwitcher(): void {
   const app: HTMLDivElement | any =
     document.querySelector<HTMLDivElement>("#app");
 
-  let currentTheme: string = localStorage.getItem("currentTheme") || "light";
+  // let currentTheme: string = localStorage.getItem("currentTheme") || "light";
 
   const theme_switcher: HTMLDivElement | any =
     document.querySelector<HTMLDivElement>(".theme_switcher");
